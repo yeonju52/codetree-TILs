56126e2d4659701202b7fc95dbcb3752f94eb05d
yeonjuLee <yeonju509@gmail.com>
2025-04-09 00:01:27 +0900

    Add a record in Codetree
    Add a record in Codetree
    
    Title: 메두사와 전사들, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 15ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 15ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 15ms, Memory: 0MB, Status: Wrong Answer - Codetree
    
    Title: 메두사와 전사들, Time: 14ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250409/메두사와 전사들/medusa-and-warriors.cpp
@@ -0,0 +1,280 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define first X
+#define second Y
+
+#include<iostream>
+#include <cstdio>
+#include<cstring>
+#include<vector>
+#include<queue>
+using namespace std;
+
+const int MAX = 50;
+int N, M;
+
+struct pos {
+	int x, y, status;
+};
+
+pair<int, int> st, ed;
+
+int board[MAX][MAX];
+int area[MAX][MAX];
+int vis[MAX][MAX];
+pos warrior[300];
+
+// 상하좌우
+int dx[8] = { -1, 1, 0, 0, -1, -1, 1, 1 };
+int dy[8] = { 0, 0, -1, 1, -1, 1, -1, 1 };
+
+bool bfs();
+void bfs_back();
+int get_dir(int x, int y, int ex, int ey, int mode);
+void view(int x, int y, int val, const vector<int> &dir);
+void display(int arr[MAX][MAX]);
+
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int test_case;
+	int T;
+
+	//freopen("input.txt", "r", stdin);
+	//cin >> T;
+	T = 1;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		memset(board, 0, sizeof(board));
+		memset(vis, 0, sizeof(vis));
+		memset(warrior, 0, sizeof(warrior));
+
+		cin >> N >> M;
+		cin >> st.X >> st.Y >> ed.X >> ed.Y;
+
+		for (int w = 0; w < M; w++) {
+			int x, y;
+			cin >> x >> y;
+			warrior[w] = { x, y, 0 };
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cin >> board[i][j];
+			}
+		}
+
+		// 만약 메두사가 도착 못하는 경우, -1
+		if (!bfs()) {
+			cout << -1 << "\n";
+			break;
+		}
+		else {
+			bfs_back();
+		}
+
+		int t = 0, moveCnt = 0, rockCnt = 0, attackCnt = 0;
+		int x = st.X, y = st.Y;
+		while (true) {
+			t++;
+			// 1. 메두사 이동
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i], ny = y + dy[i];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+				if (vis[nx][ny] == vis[x][y] + 1) {
+					x = nx, y = ny;
+					break;
+				}
+			}
+
+			if (x == ed.X && y == ed.Y) {
+				cout << "0" << "\n";
+				break;
+			}
+
+			// 전사가 있으면 공격
+			for (int w = 0; w < M; w++) {
+				pos &war = warrior[w];
+				if (war.x == x && war.y == y) war.status = -1; // .erase(warrior.begin() + i);
+			}
+
+			// 2. 메두사의 시선
+			// 상(0, 4, 5), 하(1, 6, 7) 좌(2, 4, 6), 우(1, 3, 5)
+			vector<vector<int>> dir = { {0, 4, 5}, {1, 6, 7}, {2, 4, 6}, {3, 5, 7} };
+
+			int cnt = 0, find_d = 0;
+			rockCnt = 0;
+			bool real = false;
+			for (int i = 0; i < 5; i++) { // 4 + 1(정한 find_d로)
+				memset(area, 0, sizeof(area)); // 메두사 영역 초기화
+
+				if (i == 4) {
+					i = find_d; real = true;
+				}
+				// 1) area: 메두사 그림자 영역 1 체크
+				view(x, y, 1, dir[i]);
+				// 2) area: 전사 그림자 영역 3 체크
+				for (int w = 0; w < M; w++) {
+					pos &war = warrior[w];
+					if (area[war.x][war.y] != 1) continue; // 1일 때만 신경 써야
+					if (war.status) continue;
+					if (i == 0 && war.x < x) {
+						if (war.y < y) view(war.x, war.y, 3, { 0, 4 });
+						else if (war.y == y) view(war.x, war.y, 3, { 0 });
+						else view(war.x, war.y, 3, { 0, 5 });
+					}
+					else if (i == 1 && war.x > x) {
+						if (war.y < y) view(war.x, war.y, 3, { 1, 6 });
+						else if (war.y == y) view(war.x, war.y, 3, { 1 });
+						else view(war.x, war.y, 3, { 1, 7 });
+					}
+					else if (i == 2 && war.y < y) {
+						if (war.x < x) view(war.x, war.y, 3, { 2, 4 });
+						else if (war.x == x) view(war.x, war.y, 3, { 2 });
+						else view(war.x, war.y, 3, { 2, 6 });
+					}
+					else if (i == 3 && war.y > y) {
+						if (war.x < x) view(war.x, war.y, 3, { 3, 5 });
+						else if (war.x == x) view(war.x, war.y, 3, { 3 });
+						else view(war.x, war.y, 3, { 3, 7 });
+					}
+				}
+				if (!real) {
+					cnt = 0;
+					for (int w = 0; w < M; w++) {
+						pos &war = warrior[w];
+						if (!war.status && area[war.x][war.y] == 1) cnt++;
+					}
+					if (rockCnt < cnt) {
+						rockCnt = cnt;
+						find_d = i;
+					}
+				}
+				else { // 실제 find_d로 전사 돌 만들기
+					for (int w = 0; w < M; w++) {
+						pos &war = warrior[w];
+						if (!war.status && area[war.x][war.y] == 1) war.status = 1;
+					}
+					break;
+				}
+			}
+
+			// 3. 전사들의 이동
+			moveCnt = 0; // 이동횟수
+			attackCnt = 0;
+			for (int w = 0; w < M; w++) {
+				pos &war = warrior[w];
+				for (int k = 0; k < 2; k++) {
+					if (war.status || area[war.x][war.y] == 1) break; // war.status (1:돌, -1:죽음) 일 때
+					int i = get_dir(war.x, war.y, x, y, k * 2); // 전사들 모드 (0, 1 상관없이 모두 지나감)
+					if (i == -1) break;
+					war.x += dx[i], war.y += dy[i];
+					moveCnt++;
+					if (war.x == x && war.y == y) {
+						attackCnt++;
+						war.status = -1;
+						break;
+					}
+				}
+			}
+
+			cout << moveCnt << " " << rockCnt << " " << attackCnt << "\n";
+			for (int w = 0; w < M; w++) {
+				pos &war = warrior[w];
+				if (war.status == 1) war.status = 0;
+			}
+		}
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
+
+int get_dir(int x, int y, int ex, int ey, int base) {
+	int dir = -1;
+	int minval = N * N;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[(base + i) % 4], ny = y + dy[(base + i) % 4];
+		if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+		if (area[nx][ny] == 1) continue;
+		int val = abs(nx - ex) + abs(ny - ey);
+		if (val >= abs(x - ex) + abs(y - ey)) continue;
+		if (minval > val) {
+			minval = val;
+			dir = (base + i) % 4;
+		}
+	}
+	return dir;
+}
+
+void view(int x, int y, int val, const vector<int> &dir) {
+	queue<pair<int, int>> q;
+	q.push({ x, y });
+	//area[x][y] = val; // 체킹하면 안됨
+
+	int len = dir.size();
+	while (!q.empty()) {
+		pair<int, int> cur = q.front(); q.pop();
+		x = cur.X, y = cur.Y;
+		for (int i = 0; i < len; i++) {
+			int nx = x + dx[dir[i]], ny = y + dy[dir[i]];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (area[nx][ny] != val) {
+				q.push({ nx, ny });
+				area[nx][ny] = val;
+			}
+		}
+	}
+}
+
+bool bfs() {
+	queue<pair<int, int>> q;
+
+	int x = st.X, y = st.Y;
+	vis[x][y] = 1;
+	q.push({ x, y });
+
+	while (!q.empty()) {
+		pair<int, int> cur = q.front(); q.pop();
+		x = cur.X, y = cur.Y;
+		if (x == ed.X && y == ed.Y) return true;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (board[nx][ny] == 1) continue;
+			if (vis[nx][ny] > 0) continue;
+			vis[nx][ny] = vis[x][y] + 1;
+			q.push({ nx, ny });
+		}
+	}
+	return false;
+}
+
+int backup[MAX][MAX];
+void bfs_back() {
+	memcpy(backup, vis, sizeof(backup));
+	memset(vis, 0, sizeof(vis));
+
+	int x = ed.X, y = ed.Y;
+	vis[x][y] = backup[x][y];
+	while (true) {
+		if (x == st.X && y == st.Y) return;
+		for (int i = 3; i >= 0; i--) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (backup[nx][ny] == backup[x][y] - 1) {
+				vis[nx][ny] = backup[nx][ny];
+				x = nx, y = ny;
+				break;
+			}
+		}
+	}
+}
+
+void display(int arr[MAX][MAX]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << arr[i][j];
+		}
+		cout << "\n";
+	}
+}
